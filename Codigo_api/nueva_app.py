c5e635287a4e762391cd9bbc314b95759684f3b7
Lorenhdc <95498391+Lorenhdc@users.noreply.github.com>
2022-05-11 17:24:21 +0200

    cambios main

--- Codigo_api/nueva_app.py
+++ Codigo_api/nueva_app.py
@@ -1,17 +1,12 @@
-from re import X
-from urllib import response
 from flask import Flask, jsonify, request
 import os
 import pickle
-from jsonschema import draft201909_format_checker
 from sklearn.linear_model import Lasso
 import pandas as pd
 from sklearn.model_selection import train_test_split
-import sqlite3 as sql
-from sklearn.metrics import mean_absolute_error, mean_squared_error, mean_absolute_percentage_error
-
 
 os.chdir(os.path.dirname(__file__))
+
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -20,11 +15,35 @@ model = pickle.load(open('ad_model.pkl','rb'))
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>GRUPO 3 THE BRIDGE</h1><p>Home Link.</p>"
-
-@app.route('/estamosjodidos', methods=['GET'])
-def home():
-    return "<h1>GRUPO 3 THE BRIDGE</h1><p>This link is made to show our users predictions.</p>"
+    return "<h1>Grupo 3 THE BRIDGE</h1><p>Home Link.</p>"
 
 
-#app.run()
+@app.route('/api/v1/predict', methods=['GET'])
+def predict():
+
+    tv = request.args.get('tv', None)
+    radio = request.args.get('radio', None)
+    newspaper = request.args.get('newspaper', None)
+
+    if tv is None or radio is None or newspaper is None:
+        return "Args empty, the data are not enough to predict"
+    else:
+        prediction = model.predict([[tv,radio,newspaper]])
+
+    return jsonify({'predictions': prediction[0]})
+
+@app.route('/api/v1/retrain', methods=['GET'])
+def train():
+    data = pd.read_csv('data/Advertising.csv', index_col=0)
+
+    X_train, X_test, y_train, y_test = train_test_split(data.drop(columns=['sales']),
+                                                        data['sales'],
+                                                        test_size = 0.05,
+                                                        random_state=42)
+
+    model = Lasso(alpha=6000)
+    model.fit(X_train, y_train)
+
+    pickle.dump(model, open('ad_model.pkl', 'wb'))
+
+    return "model trained and saved"
